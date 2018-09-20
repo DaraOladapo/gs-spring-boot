4523245b1ad44440cf9596311bdd0bf24188db2e
Dara Oladapo <daraoladapo@outlook.com>
2018-09-20 09:52:57 +0100

    back to start

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "!-edited + mini edit-Greetings from Spring Boot!";
+        return "Greetings from Spring Boot!";
     }
     
 }
